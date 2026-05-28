7ae39cb953270d3edadff56bed93d9216c6efcf2
César Ribeiro <107416178+cmribeiro96@users.noreply.github.com>
2026-05-28 01:56:56 -0300

    Add files via upload

--- js/api.js
+++ js/api.js
@@ -1,92 +1,110 @@
-const URL_BASE = 'http://localhost:3000'
+const URL_BASE = "http://localhost:3000"
+
+const converterStringParaData = (dataString) => {
+  const [ano, mes, dia] = dataString.split('-').map(Number)
+  return new Date(Date.UTC(ano, mes-1, dia))
+  //2026-05-27 = [2026, 05, 27]
+}
 
 const api = {
-    async buscarPensamentos() {
-        try {
-            //sem axios: const response = await fetch(`${URL_BASE}/pensamentos`)
-            const response = await axios.get(`${URL_BASE}/pensamentos`) //com axios
-
-            //sem axios: return response.json()
-            return response.data //com axios
+  async buscarPensamentos() {
+    try {
+      const response = await axios.get(`${URL_BASE}/pensamentos`)
+      const pensamentos = await response.data
+      
+      return pensamentos.map(pensamento => {
+        return {
+          ...pensamento,
+          data: new Date(pensamento.data)
         }
-        catch (error) {
-            alert('Erro ao buscar pensamentos')
-            throw error
-        }
-    },
-    async salvarPensamento(pensamento) {
-        try {            
-            //sem axios: 
-            //const response = await fetch(`${URL_BASE}/pensamentos`, {            
-            //     method:'POST', 
-            //     headers: {
-            //         "Content-Type": "application/json"
-            //     },
-            //     body: JSON.stringify(pensamento)
-            // })
-            //return await response.json()
-
-            //com axios:            
-            const response = await axios.post(`${URL_BASE}/pensamentos`, pensamento) 
-            return await response.data 
-        }
-        catch (error) {
-            alert('Erro ao salvar pensamento')
-            throw error
-        }
-    },
-    async buscarPensamentoPorId(id) {
-        try {
-            //sem axios:
-            // const response = await fetch(`${URL_BASE}/pensamentos/${id}`)
-            // return await response.json()            
-
-            //com axios:
-            const response = await axios.get(`${URL_BASE}/pensamentos/${id}`)
-            return await response.data
-        }
-        catch (error) {
-            alert('Erro ao buscar pensamento por id')
-            throw error
-        }
-    },
-    async editarPensamento(pensamento) {
-        try {
-            //sem axios:
-        //     const response = await fetch(`${URL_BASE}/pensamentos/${pensamento.id}`, {
-        //         method:'PUT', 
-        //         headers: {
-        //             "Content-Type": "application/json"
-        //         },
-        //         body: JSON.stringify(pensamento)
-        //     })
-        //     return await response.json()
-        // }
-
-        //com axios:
-        const response = await axios.put(`${URL_BASE}/pensamentos/${pensamento.id}`, pensamento)
-        return await response.data
-        }
-        catch (error) {
-            alert('Erro ao editar pensamento')
-            throw error
-        }
-    },
-    async excluirPensamento(id) {
-        try {
-            //sem axios:
-            // await fetch(`${URL_BASE}/pensamentos/${id}`, {
-            //     method:'DELETE'
-            // })
-
-            //com axios:
-            await axios.delete(`${URL_BASE}/pensamentos/${id}`)
-        }
-        catch (error) {
-            throw error
-        }
+      })
     }
+    catch {
+      alert('Erro ao buscar pensamentos')
+      throw error
+    }
+  },
+
+  async salvarPensamento(pensamento) {
+    try {
+      const data = converterStringParaData(pensamento.data)
+      
+      const response = await axios.post(`${URL_BASE}/pensamentos`, {
+        ...pensamento,
+        data: data.toISOString()
+      })
+      return await response.data
+    }
+    catch {
+      alert('Erro ao salvar pensamento')
+      throw error
+    }
+  },
+
+  async buscarPensamentoPorId(id) {
+    try {
+      const response = await axios.get(`${URL_BASE}/pensamentos/${id}`)
+      const pensamento = await response.data
+
+      return {
+        ...pensamento,
+        data: new Date(pensamento.data)
+      }
+    }
+    catch {
+      alert('Erro ao buscar pensamento')
+      throw error
+    }
+  },
+
+  async editarPensamento(pensamento) {
+    try {
+      const response = await axios.put(`${URL_BASE}/pensamentos/${pensamento.id}`, pensamento)
+      return await response.data
+    }
+    catch {
+      alert('Erro ao editar pensamento')
+      throw error
+    }
+  },
+
+  async excluirPensamento(id) {
+    try {
+      const response = await axios.delete(`${URL_BASE}/pensamentos/${id}`)
+    }
+    catch {
+      alert('Erro ao excluir um pensamento')
+      throw error
+    }
+  },
+
+  async buscarPensamentosPorTermo(termo){
+    try{
+      const pensamentos = await this.buscarPensamentos()
+      const termoEmMinusculas = termo.toLowerCase()
+  
+      const pensamentosFiltrados = pensamentos.filter(pensamento => {
+        return (pensamento.conteudo.toLowerCase().includes(termoEmMinusculas) || pensamento.autoria.toLowerCase().includes(termoEmMinusculas))
+      })
+      return pensamentosFiltrados
+    }catch (error){
+      alert('Erro ao buscar pensamentos')
+      throw error
+    }
+
+  },
+
+  async atualizarFavorito(id, favorito) {
+    try {
+      const response = await axios.patch(`${URL_BASE}/pensamentos/${id}`, { favorito })
+      return await response.data
+    }
+    catch(error) {
+      alert('Erro ao atualizar favorito')
+      throw error
+    }
+  }
 
 }
 
-export default api;
+export default api
--- js/main.js
+++ js/main.js
@@ -1,36 +1,118 @@
 import ui from "./ui.js"
 import api from "./api.js"
 
+const pensamentosSet = new Set()
+
+async function adicionarChaveAoPensamento(){
+  try{
+    const pensamentos = await api.buscarPensamentos()
+    pensamentos.forEach(pensamento => {
+      const chavePensamento = `${pensamento.conteudo.trim().toLowerCase()}-${pensamento.autoria.trim().toLowerCase()}`
+      pensamentosSet.add(chavePensamento)
+    })
+  }
+  catch(error){
+    alert('Erro ao adicionar chave ao pensamento')
+  }
+}
+
+
+function removerEspaços(string){
+  return string.replaceAll(/\s+/g, '')
+}
+
+const regexConteudo = /^[\p{L}\s.,!?;:()'"\-]{10,}$/u
+
+function validarConteudo(conteudo) {
+  return regexConteudo.test(conteudo)
+}
+
+const regexAutoria = /^[\p{L}\s]{3,30}$/u
+
+function validarAutoria(autoria) {
+  return regexAutoria.test(autoria)
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-    ui.renderizarPensamentos()
+  ui.renderizarPensamentos()
+  adicionarChaveAoPensamento()
 
-    const formularioPensamento = document.getElementById("formulario-pensamento")
-    const botaoCancelar = document.getElementById("botao-cancelar")
+  const formularioPensamento = document.getElementById("pensamento-form")
+  const botaoCancelar = document.getElementById("botao-cancelar")
+  const inputBusca = document.getElementById("campo-busca")
 
-    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
-    botaoCancelar.addEventListener("click", manipularCancelamento)
+
+  formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+  botaoCancelar.addEventListener("click", manipularCancelamento)
+  inputBusca.addEventListener("input", manipularBusca)
 })
 
 async function manipularSubmissaoFormulario(event) {
-    event.preventDefault();
-    const id = document.getElementById("pensamento-id").value
-    const conteudo = document.getElementById("pensamento-conteudo").value
-    const autoria = document.getElementById("pensamento-autoria").value
+  event.preventDefault()
+  const id = document.getElementById("pensamento-id").value
+  const conteudo = document.getElementById("pensamento-conteudo").value
+  const autoria = document.getElementById("pensamento-autoria").value
+  const data = document.getElementById("pensamento-data").value
 
-    try {
-        if(id){
-            await api.editarPensamento({ id, conteudo, autoria })            
-        } else {            
-            await api.salvarPensamento({ id, conteudo, autoria })
-        }
-        ui.renderizarPensamentos()
-    }
-    catch (error) {
-        alert('Erro ao salvar pensamento')
-    }
+  const conteudoSemEspaços = removerEspaços(conteudo)
+  const autoriaSemEspaços = removerEspaços(autoria)
 
+  if (!conteudoSemEspaços || !autoriaSemEspaços || !data) {
+    alert("Todos os campos são obrigatórios. Somente espaços em branco ou campos vazios não são considerados preenchimentos válidos.")
+    return
+  }
+
+  const chaveNovoPensamento = `${conteudo.trim().toLowerCase()}-${autoria.trim().toLowerCase()}`
+
+  if (pensamentosSet.has(chaveNovoPensamento)) {
+    alert("Já existe um pensamento com o mesmo conteúdo e autoria. Por favor, insira um pensamento diferente.")
+    return
+  }
+
+  if (!validarConteudo(conteudo)) {
+    alert("O conteúdo deve conter no mínimo 10 caracteres e aceitar apenas letras, espaços e pontuação comum (.,!?;:()'\"-).")
+    return
+  }
+
+  if (!validarAutoria(autoria)) {
+    alert("A autoria deve conter entre 3 e 30 caracteres, aceitando apenas letras e espaços.")
+    return
+  }
+
+  if (!validarData(data)) {
+    alert("Não é permitido o cadastro de datas futuras. Selecione outra data.")
+    return
+  }
+
+  try {
+    if (id) {
+      await api.editarPensamento({ id, conteudo, autoria, data })
+    } else {
+      await api.salvarPensamento({ conteudo, autoria, data })
+    }
+    ui.renderizarPensamentos()
+  } catch {
+    alert("Erro ao salvar pensamento")
+  }
 }
 
 function manipularCancelamento() {
-    ui.limparFormulario();
+  ui.limparFormulario()
+}
+
+async function manipularBusca() {
+  const termoBusca = document.getElementById("campo-busca").value
+  try {
+    const pensamentosFiltrados = await api.buscarPensamentosPorTermo(termoBusca)
+    ui.renderizarPensamentos(pensamentosFiltrados)
+    //console.log(pensamentosFiltrados)
+  } catch (error) {
+    alert("Erro ao realizar busca")
+  }
+}
+
+function validarData(data){
+  const dataAtual = new Date();
+  const dataInserida = new Date(data);
+  return dataInserida <= dataAtual;
 }
